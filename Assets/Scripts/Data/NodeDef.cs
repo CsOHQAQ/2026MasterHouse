338db68f7ee800a4aeb522db0b1ce18e52fd304f
CsOHQAQ <529353999@qq.com>
2026-07-30 20:53:41 +0800

    正在重构系统数据结构

--- Assets/Scripts/Data/NodeDef.cs
+++ Assets/Scripts/Data/NodeDef.cs
@@ -1,13 +1,12 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace MasterPotion
+enum ENodeType
 {
-    /// <summary>节点卡片的静态定义基类。</summary>
-    public abstract class NodeDef : ScriptableObject
-    {
-        public string displayName;
-        [Tooltip("卡片尺寸（世界单位）")]
-        public Vector2 size = new Vector2(2.4f, 3f);
-        public Color cardColor = new Color(0.22f, 0.25f, 0.3f);
-    }
+    None,
+    Resource,
+}
+
+public class NodeDef
+{
+    
 }
