5cd762dbdb1ea1bf1ae7b03a38a7979219a80bde
CsOHQAQ <529353999@qq.com>
2026-07-31 00:19:50 +0800

    *基本没怎么改

--- Assets/Scripts/Data/NodeDef.cs
+++ Assets/Scripts/Data/NodeDef.cs
@@ -1,12 +1,25 @@
 ﻿using UnityEngine;
 
-enum ENodeType
+namespace Data
 {
-    None,
-    Resource,
-}
+    public enum ENodeType
+    {
+        None,   //出现这个说明你忘了配置
+        Resource,
+        Processor,
+        Collector,
+    }
 
-public class NodeDef
-{
-    
+    // Pure DataStruct
+    public class NodeDef : ScriptableObject
+    {
+        public readonly ENodeType NodeType = ENodeType.None;
+    }
+
+
+    public class ResourceNodeDef : NodeDef
+    {
+        public readonly ENodeType NodeType = ENodeType.Resource;
+        public readonly string NodeName;
+    }
 }
